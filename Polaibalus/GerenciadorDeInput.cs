688b85e24f9aaa88529a70d210bf2d388bb84cad
Celso Bielskis <celsobielskis@gmail.com>
2018-11-10 03:04:39 -0200

    First

---
+++ Polaibalus/GerenciadorDeInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atari_II
+{
+
+    class GerenciadorDeInput
+    {
+        public static ConsoleKey teclaApertada;
+
+        public void ChecaInput()
+        {
+            if (Console.KeyAvailable)
+            {
+                teclaApertada = Console.ReadKey(true).Key;
+            }
+
+            else
+            {
+                teclaApertada = 0;
+            }
+        }
+    }
+}
---
+++ Polaibalus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atari_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.CursorVisible = false;
+
+            Jogo meuJogo = new Jogo(80,18);
+          
+
+            while (meuJogo.rodando)
+            {
+                meuJogo.Update();
+            }
+            
+        }
+    }
+}
